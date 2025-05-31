64ba269e8ed4fd078d8ebb4473af37dd731753e4
jiyoon <ljy10499@gmail.com>
2025-05-31 20:34:06 +0900

    cp2

---
+++ flutter_recipe/android/app/src/main/kotlin/com/example/flutter_recipe/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_recipe
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ flutter_recipe/lib/components/recipe_list_item.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class RecipeListItem extends StatelessWidget {
+  final String imageName;
+  final String title;
+
+  const RecipeListItem(this.imageName, this.title);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
